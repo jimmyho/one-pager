a746ab475ab6d1162cc3487eed9087af0cebf86b
James Ho <jimmy@mac.local>
2012-12-15 16:19:23 +0000

    update menu bar to enable scrollto elements

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 
+$(document).ready(function($) {
+
+    $('.nav a').click(function(event){
+        event.preventDefault(); 
+
+        var to_elem = $(this).attr('href')
+        if (to_elem && $(to_elem).length == 1 ) {
+            $.scrollTo($(to_elem), 800)
+        } else {
+            console.log('Element not found: '+ to_elem);
+        }
+    });
+
+});
